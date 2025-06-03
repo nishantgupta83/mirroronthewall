dc9ec1cb8752e2664c1469d4d7dd5f14211dde6e
nishantgupta83 <65264123+nishantgupta83@users.noreply.github.com>
2025-06-03 13:46:32 -0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,8 +4,15 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+// backend/index.js
+const cors = require("cors");
+
 app.use(cors());
 app.use(express.json());
+
+app.use(cors({
+  origin: "https://mirroronthewall-frontend.onrender.com"
+}));
 
 let messages = [
   {
@@ -34,3 +41,5 @@ app.post('/api/messages', (req, res) => {
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
