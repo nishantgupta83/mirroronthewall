cf88cef6bb904e7a1081e76882b5c3086ea707e7
nishantgupta83 <65264123+nishantgupta83@users.noreply.github.com>
2025-06-03 13:04:55 -0700

    Create service-worker.js

---
+++ frontend/public/service-worker.js
@@ -0,0 +1,7 @@
+self.addEventListener("install", function (event) {
+  console.log("Service Worker installed.");
+});
+
+self.addEventListener("fetch", function (event) {
+  // Bypass cache for now
+});
